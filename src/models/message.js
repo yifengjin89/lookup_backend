69dc2673fa95df9794a3ab02393a33e110a652be
yifengjin89 <55426354+yifengjin89@users.noreply.github.com>
2021-03-19 21:45:31 -0400

    added send friend request functionailty

--- src/models/message.js
+++ src/models/message.js
@@ -1,20 +1,36 @@
 const mongoose = require('mongoose');
 
-const messageSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User'
-  },
-  send:[{
-    receiverId:String,
-    receiver:String,
+const MessageSchema = new mongoose.Schema({
+    from_userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    },
+
+    to_userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    }, 
     
-  }],
-  receive:[{
+    from_username: {
+      type: String,
+      ref: 'User',
+    },
 
-  }],
+    from_profileImage: {
+      type: String,
+      ref: 'User',
+    },
 
-})
+    msgType: {
+      type: String,
+    },
 
-module.exports = mongoose.model('Messages', messageSchema);
+    status:{
+        type: String,
+        default: 'Pending',
+    },
+
+}, {timestamps: true});
+
+module.exports = mongoose.model('Message', MessageSchema);
